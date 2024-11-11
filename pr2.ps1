98dfd42bd6a030b9e60ecefba1b94be296b729fd
Mairo <mairo.kunnap@voco.ee>
2024-11-11 11:25:08 +0200

    praks2 hashtables

---
+++ pr2.ps1
@@ -0,0 +1,7 @@
+﻿# initial hashtables
+$worked=@{Name="John","Joe","Mary"; DaysWorked=12, 20, 18}
+$salaryday=@{Name="John","Joe","Mary"; SalaryPerDay=100, 120, 150}
+# new hashtable
+$salary=@{Name="John","Joe","Mary"; Salary=($worked.DaysWorked[0] * $salaryday.SalaryPerDay[0]),($worked.DaysWorked[1] * $salaryday.SalaryPerDay[1]),($worked.DaysWorked[2] * $salaryday.SalaryPerDay[2])}
+# print results
+$salary
